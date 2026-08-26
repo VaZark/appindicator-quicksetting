655fd13997a6f6e92448cb3d360f3c2f98527ce5
Vasanth M <vasanth93067@gmail.com>
2026-08-26 11:30:41 +0200

    Fullwidth dropdown

--- extension.js
+++ extension.js
@@ -23,37 +23,53 @@ import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/ex
 import {QuickToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
 
 
+const RunningAppsToggle = GObject.registerClass(
+class RunningAppsToggle extends QuickToggle {
+    _init() {
+        super._init({
+            hasMenu: true,
+            iconName: 'go-next-symbolic',
+        });
 
-const ExampleToggle = GObject.registerClass(
-class ExampleToggle extends QuickToggle {
-    constructor() {
-        super({
-            title: _('Running Apps'),
-            iconName: 'preferences-desktop-multitasking-symbolic',
-            toggleMode: true,
+        this.add_style_class_name('background-apps-quick-toggle');
+        this._box.set_child_above_sibling(this._icon,null);
+        this.title = _('Running Apps');
+        this.menu.setHeader(
+            'preferences-desktop-multitasking-symbolic',
+            _('Running Apps')
+        );
+
+        this.connect('popup-menu', () => {
+            this.menu.open();
         });
     }
-});
 
-const ExampleIndicator = GObject.registerClass(
-class ExampleIndicator extends SystemIndicator {
-    constructor() {
-        super();
-
-        this._indicator = this._addIndicator();
-        this._indicator.iconName = 'preferences-desktop-multitasking-symbolic';
-
-        const toggle = new ExampleToggle();
-        toggle.bind_property('checked',
-            this._indicator, 'visible',
-            GObject.BindingFlags.SYNC_CREATE);
-        this.quickSettingsItems.push(toggle);
+    vfunc_clicked() {
+        this.menu.open();
     }
+
 });
 
-export default class QuickSettingsExampleExtension extends Extension {
+
+const RunningAppsIndicator = GObject.registerClass(
+class RunningAppsIndicator extends SystemIndicator {
+  constructor() {
+      super();
+
+      this._indicator = this._addIndicator();
+      this._indicator.iconName = 'preferences-desktop-multitasking-symbolic';
+
+      const toggle = new RunningAppsToggle();
+      toggle.bind_property('checked',
+          this._indicator, 'visible',
+          GObject.BindingFlags.SYNC_CREATE);
+      this.quickSettingsItems.push(toggle);
+  }
+});
+
+export default class RunningAppsExtension extends Extension {
     enable() {
-        this._indicator = new ExampleIndicator();
+        this._indicator = new RunningAppsIndicator();
         Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
     }
 
